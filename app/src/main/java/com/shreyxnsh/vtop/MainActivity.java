30c2a6e9e638f7e5809212aaea1c061d1f89251d
shreyxnsh <shreyanshatwork@gmail.com>
2023-01-17 15:11:09 +0530

    Navigation Drawer with a new look!

--- app/src/main/java/com/shreyxnsh/vtop/MainActivity.java
+++ app/src/main/java/com/shreyxnsh/vtop/MainActivity.java
@@ -3,12 +3,17 @@ package com.shreyxnsh.vtop;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -38,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ActionBarDrawerToggle toggle;
     private NavigationView navigationView;
     private Uri uri;
+    ConstraintLayout contentView;
+    static final float END_SCALE = 0.7f;
 
 
     @Override
@@ -48,25 +55,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navigation_view);
+        contentView = findViewById(R.id.contentView);
 
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        navigationView.setNavigationItemSelectedListener(this);
 
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        navigation();
+        bottomNavigation();
 
-        //bottom nav code for setting id and icons
-        bottomNavigationView.add(new MeowBottomNavigation.Model(1, R.drawable.ic_baseline_home_24));
-        bottomNavigationView.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_notifications_active_24));
-        bottomNavigationView.add(new MeowBottomNavigation.Model(3, R.drawable.ic_baseline_people_24));
-        bottomNavigationView.add(new MeowBottomNavigation.Model(4, R.drawable.ic_baseline_photo_library_24));
-        bottomNavigationView.add(new MeowBottomNavigation.Model(5, R.drawable.ic_baseline_info_24));
-
-        // after starting app, show first fragment
-        replace(new HomeFragment());
-        bottomNavigationView.show(1,true);
 
         //bottom nav code for onClickListener
         bottomNavigationView.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
@@ -105,12 +103,61 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+    private void bottomNavigation() {
+        //bottom nav code for setting id and icons
+        bottomNavigationView.add(new MeowBottomNavigation.Model(1, R.drawable.ic_baseline_home_24));
+        bottomNavigationView.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_notifications_active_24));
+        bottomNavigationView.add(new MeowBottomNavigation.Model(3, R.drawable.ic_baseline_people_24));
+        bottomNavigationView.add(new MeowBottomNavigation.Model(4, R.drawable.ic_baseline_photo_library_24));
+        bottomNavigationView.add(new MeowBottomNavigation.Model(5, R.drawable.ic_baseline_info_24));
+
+        // after starting app, show first fragment
+        replace(new HomeFragment());
+        bottomNavigationView.show(1,true);
+    }
+
+    private void navigation() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        animateNavigationView();
+    }
+
+    private void animateNavigationView() {
+        //Add any color or remove it to use the default one!
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        drawerLayout.setScrimColor(R.color.dark_blue);
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
     private void replace(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout,fragment);
         fragmentTransaction.commit();
-
-
 
     }
 
