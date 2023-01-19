5a0bcde69eadd1b4da81b231baaf0974d66013b8
shreyxnsh <shreyanshatwork@gmail.com>
2023-01-20 01:12:09 +0530

    Developers activity implemented

--- app/src/main/java/com/shreyxnsh/vtop/ui/developers/Developers.java
+++ app/src/main/java/com/shreyxnsh/vtop/ui/developers/Developers.java
@@ -1,16 +1,26 @@
 package com.shreyxnsh.vtop.ui.developers;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.shreyxnsh.vtop.R;
 
 public class Developers extends AppCompatActivity {
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developers);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("  Developers");
+        actionBar.setIcon(R.drawable.developer_white);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
     }
 }
