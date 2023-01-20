28e93f570f43512dcc1dfc09243a5a43b143d50c
shreyxnsh <shreyanshatwork@gmail.com>
2023-01-21 02:30:13 +0530

    Developers activity updated

--- app/src/main/java/com/shreyxnsh/vtop/ui/developers/Developers.java
+++ app/src/main/java/com/shreyxnsh/vtop/ui/developers/Developers.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.shreyxnsh.vtop.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Developers extends AppCompatActivity {
+
+    private Uri uri;
+    private ImageView gmail;
+    private CircleImageView linkedin;
+    private CircleImageView github;
+    private Intent intent;
 
     @SuppressLint("RestrictedApi")
     @Override
@@ -22,5 +34,35 @@ public class Developers extends AppCompatActivity {
         actionBar.setIcon(R.drawable.developer_white);
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        gmail = findViewById(R.id.gmail);
+        linkedin = findViewById(R.id.linkedin);
+        github = findViewById(R.id.github);
+
+        //buttons to visit the links
+        gmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uri = Uri.parse(getString(R.string.gmaillink));
+                intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uri = Uri.parse(getString(R.string.linkedinlink));
+                intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uri = Uri.parse(getString(R.string.githublink));
+                intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
