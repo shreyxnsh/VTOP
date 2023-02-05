eea6d8a73d331d3dc16902721bcc7f00f07fc095
shreyxnsh <shreyanshatwork@gmail.com>
2023-02-05 22:03:24 +0530

    FeedbackActivity created

---
+++ app/src/main/java/com/shreyxnsh/vtop/ui/feedbacks/FeedbackActivity.java
@@ -0,0 +1,16 @@
+package com.shreyxnsh.vtop.ui.feedbacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shreyxnsh.vtop.R;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
